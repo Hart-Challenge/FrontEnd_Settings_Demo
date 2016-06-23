18eca41b8919bee37f32025108b4b80dc5d09a68
John Anthony Rivera <askinganthony@gmail.com>
2016-06-22 21:51:31 -0700

    Added thin class for ht-button

--- src/app/features/UI/ht-button.directive.js
+++ src/app/features/UI/ht-button.directive.js
@@ -1,13 +1,21 @@
 function htButton() {
     return {
         restrict: 'E',
+        scope: true,
         transclude: true,
-        controller: ['$scope', function($scope){
-            var ctrl = this; 
+        controller: ['$scope', '$timeout', function($scope, $timeout){
+            $scope.click = function() {
+                $scope.clicked = true;
+                $timeout(function(){
+                    $scope.clicked = false;
+                }, 1000);
+            }
         }],
         template: [
             '<form>',
-                '<button class="button-square"><ng-transclude></ng-transclude></button>',
+                '<button ng-click="click()" class="button-square" ng-class="{\'clicked\':clicked}">',
+                    '<ng-transclude></ng-transclude>',
+                '</button>',
             '</form>'
         ].join('')
     };
